b1111e130916b18fd6886d2c6dd745e73d5cf1a5
KyleTruong <ktruong008@gmail.com>
2023-04-22 19:03:06 -0400

    Init supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { supabase } from '$lib/core/supabase/supabase-client';
+
+	onMount(async () => {
+		const { data } = await supabase.from('settings').select();
+		console.log('data: ', data);
+	});
+</script>
+
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
